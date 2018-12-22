14ce1f440dfba9873f3ee6f43b917cb0387964b7
Abeer Ishtiaq <mabeer@hotmail.com>
2018-12-22 17:22:44 -0500

    authentication back end

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/<db_name>"
+  process.env.MONGODB_URI || "mongodb://localhost/taskManager"
 );
 
 // Start the API server
